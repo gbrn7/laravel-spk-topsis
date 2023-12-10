d2f55a4c148d0bb6c4326eda49a8b683c6acc828
gbrn7 <mraygbrn@gmail.com>
2023-12-10 17:27:01 +0700

    feat(controller): created auth controller

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,33 +8,33 @@
   </div>
   <div class="list-box  d-flex flex-column justify-content-between gap-5">
     <ul class="list-unstyled px-3 pt-3 d-flex flex-column gap-2">
-      <li class="rounded border-white rounded-2">
+      <li class="rounded {{Request::segment(2) === 'dashboard' ? 'active' : ''}} rounded-2">
         <a href="#" class="text-decoration-none px-3 py-3 rounded rounded-2 d-flex align-items-baseline"><i
             class="ri-dashboard-fill me-2"></i>Dashboard</a>
       </li>
-      <li class="rounded border-white rounded-2">
+      <li class="rounded {{Request::segment(2) === 'criteria' ? 'active' : ''}} rounded-2">
         <a href="#" class="text-decoration-none px-3 py-3 rounded rounded-2 d-flex align-items-baseline"><i
             class="bx bx-data me-2"></i>Data Kriteria</a>
       </li>
-      <li class="rounded border-white rounded-2">
+      <li class="rounded {{Request::segment(2) === 'alternatives' ? 'active' : ''}} rounded-2">
         <a href="#" class="text-decoration-none px-3 py-3 rounded rounded-2 d-flex align-items-baseline"><i
             class="ri-team-line me-2"></i>Data Alternatif</a>
       </li>
-      <li class="rounded border-white rounded-2">
+      <li class="rounded {{Request::segment(2) === 'grades' ? 'active' : ''}} rounded-2">
         <a href="#" class="text-decoration-none px-3 py-3 rounded rounded-2 d-flex align-items-baseline"><i
             class="ri-survey-line me-2"></i>Data Penilaian</a>
       </li>
-      <li class="rounded border-white rounded-2">
+      <li class="rounded {{Request::segment(2) === 'calculations' ? 'active' : ''}} rounded-2">
         <a href="#" class="text-decoration-none px-3 py-3 rounded rounded-2 d-flex align-items-baseline"><i
             class="ri-calculator-line me-2"></i></i>Data Perhitungan</a>
       </li>
-      <li class="rounded border-white rounded-2 active">
+      <li class="rounded {{Request::segment(2) === 'result' ? 'active' : ''}} rounded-2">
         <a href="#" class="text-decoration-none px-3 py-3 rounded rounded-2 d-flex align-items-baseline"><i
             class="ri-sort-asc me-2"></i></i>Data Hasil Akhir</a>
       </li>
     </ul>
     <ul class="list-unstyled px-3 d-flex flex-column gap-2">
-      <li class="rounded border-white rounded-2 ">
+      <li class="rounded {{Request::segment(2) === 'logout' ? 'active' : ''}} rounded-2 ">
         <a href="#" class="text-decoration-none px-3 py-3 rounded rounded-2 d-flex align-items-baseline"><i
             class="ri-logout-circle-line me-2"></i>Logout</a>
       </li>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,7 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::get('signin', [AuthController::class, 'index'])->name('signin');
+
+Route::group(['prefix' => 'topsis'], function(){
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
